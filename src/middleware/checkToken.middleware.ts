5cba63e6aa1324ccb4222a8d82cb501703a524ea
LenorSantos <monteirof700@gmail.com>
2024-10-14 01:04:37 -0300

    middleware checktoken configured and app.module accept req post in login without token

--- src/middleware/checkToken.middleware.ts
+++ src/middleware/checkToken.middleware.ts
@@ -1,11 +1,25 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Req, Res, Next } from '@nestjs/common';
+import { Injectable, NestMiddleware, HttpStatus } from '@nestjs/common';
 import { Response, Request, NextFunction } from "express";
+import * as jwt from "jsonwebtoken";
+require('dotenv/config');
+
+
 
 @Injectable()
 export class CheckToken implements NestMiddleware {
-  use(@Res() res: Response, @Req() req: Request, @Next() next: NextFunction) {
-    console.log("ok");
-    next();
+  use(req: Request, res: Response, next: NextFunction) {
+    // console.log(req.headers);
+    jwt.verify(req.headers.authorization.split(' ')[1], process.env.PASSWORD_JWT, { complete: true, algorithms: ['HS256'] }, (err: jwt.VerifyErrors, decoded: jwt.Jwt) => {
+      if (err) {
+        console.log(err);
+        res.status(HttpStatus.UNAUTHORIZED).send(err.message);
+      } else {
+        // realizar discusao futuramente para decisao sobre a insersao de logs no sistema
+        // os logs podem ser inseridos diretamente no middleware e a cada sucesso ou erro ocorre
+        // a manipulacao de logs dependendo do que se vai fazer no sistema
+        console.log(decoded);
+        next();
+      }
+    });
   }
 }
