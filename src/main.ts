9dd9b39e1cd91bd487c06b18bb40640d7c04c211
Carlos Henrique Santos Silveira <109424074+ByteCarlos@users.noreply.github.com>
2024-10-14 20:45:38 -0300

    feat: listen port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './module/app.module';
 import 'dotenv/config';
 
 async function bootstrap() {
+    const port: number | string = process.env.PORT_MAIN;
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(port);
 }
 bootstrap();
