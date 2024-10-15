0e9cd3737af20f98fb2cb8c168194b561e5aa724
Carlos Henrique Santos Silveira <109424074+ByteCarlos@users.noreply.github.com>
2024-10-15 02:45:52 -0300

    fix: CORS config

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,18 @@ import 'dotenv/config';
 async function bootstrap() {
     const port: number | string = process.env.PORT_MAIN;
     const app = await NestFactory.create(AppModule);
-
     // Habilitar CORS
+    const whitelist: string[] = ['http://localhost:3000', 'https://custos-seduc.netlify.app'];
     app.enableCors({
-        origin: '*',
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials: false
+        origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
+            if (whitelist.indexOf(origin as string) !== -1 || !origin) {
+                callback(null, true);
+            } else {
+                callback(new Error('NÃO PERMITIDO PELO CORS'));
+            }
+        }
     });
+
 
     await app.listen(port);
 }
