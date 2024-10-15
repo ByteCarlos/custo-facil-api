b0b5439c4e4ea8824dffc2751a40ec9ce00b0866
Carlos Henrique Santos Silveira <109424074+ByteCarlos@users.noreply.github.com>
2024-10-15 01:41:52 -0300

    feat: enable origin all CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import 'dotenv/config';
 async function bootstrap() {
     const port: number | string = process.env.PORT_MAIN;
     const app = await NestFactory.create(AppModule);
+
+    // Habilitar CORS
+    app.enableCors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+    });
+
     await app.listen(port);
 }
 bootstrap();
