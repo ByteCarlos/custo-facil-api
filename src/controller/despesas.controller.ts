46cc4f5bdbe594d6269d65a2af3b922284ce54b4
LenorSantos <monteirof700@gmail.com>
2024-10-16 01:35:58 -0300

    added method delete and search for query params

--- src/controller/despesas.controller.ts
+++ src/controller/despesas.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Req, Res, Param, ParseIntPipe, Query } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, Query, Delete } from '@nestjs/common';
 import { despesasService } from '../service';
 import { Response, Request } from "express";
 import { returnData } from '../model';
@@ -33,6 +33,26 @@ export class despesasController {
     this.loginService.getWhereDepartmentDespesa(id).then((result: returnData) => {
       if (result.status === 406 || result.status === 503) throw result;
       res.status(result.status).send(result.data);
+    }).catch((err: returnData) => {
+      res.status(err.status).send(err.message);
+    });
+  }
+
+  @Get('despesa')
+  getWhereDespesaId(@Query('id') id: number, @Res() res: Response, @Req() req: Request) {
+    this.loginService.getWhereDespesaId(id).then((result: returnData) => {
+      if (result.status === 406 || result.status === 503) throw result;
+      res.status(result.status).send(result.data);
+    }).catch((err: returnData) => {
+      res.status(err.status).send(err.message);
+    });
+  }
+
+  @Delete()
+  deleteDespesa(@Query('id') id: number, @Res() res: Response, @Req() req: Request) {
+    this.loginService.deleteDespesa(id).then((result: returnData) => {
+      if (result.status === 406 || result.status === 503) throw result;
+      res.status(result.status).send(result.message);
     }).catch((err: returnData) => {
       res.status(err.status).send(err.message);
     });
